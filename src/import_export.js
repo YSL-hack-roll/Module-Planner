cde21ee015f319ea53f5e5a711041d8eec5ebd30
lirc572 <34601854+lirc572@users.noreply.github.com>
2021-01-09 11:22:26 +0800

    update export button

--- src/import_export.js
+++ src/import_export.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-  setTimeout(() => {
-    add_button();
-  }, 500);
+  add_button();
   const url_string = window.location.href;
   const url = new URL(url_string);
   const planner = url.searchParams.get("planner");
@@ -43,11 +41,13 @@ function importData() {
 }
 
 function add_button() {
-
-
-  var export_btn = $(`<button class="btn btn-svg btn-outline-primary" style="margin-left:8px" type="button">Export</button>`);
+  var export_btn = $(`<button class="btn btn-svg btn-outline-primary" style="margin-left:8px" type="button">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg svg-small"><polyline points="17 1 21 5 17 9"></polyline><path d="M3 11V9a4 4 0 0 1 4-4h14"></path><polyline points="7 23 3 19 7 15"></polyline><path d="M21 13v2a4 4 0 0 1-4 4H3"></path></svg>
+  Export</button>`);
   var space = $('<div class="divider"/>');
-  space.appendTo($(".main-content > div > header > div"));
-  export_btn.appendTo($(".main-content > div > header > div"));
-  export_btn.click(exportData);
+  checkExist(".main-content > div > header > div", function () {
+    space.appendTo($(".main-content > div > header > div"));
+    export_btn.appendTo($(".main-content > div > header > div"));
+    export_btn.click(exportData);
+  });
 }
